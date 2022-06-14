d98a8a7c7f18f360db269d80b277f16fa5bb3ad4
Mike <mike.levy.rva@gmail.com>
2022-06-14 12:47:01 -0400

    created 'createTaskHandlerFunction'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,21 @@
 
 //  DOM object reference 
-var buttonEl = document.querySelector("#save-task");
-// task item DOM reference
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// Then we'll add the following event listener and also change the text of the new task item dynamically, using the textContent property we used previously to note, "This is a new task."
 
+var createTaskHandler = function (event) {
 
-// Create a new task item.
+    event.preventDefault();
 
-// Style the new task item.
-
-// Add the text.
-
-// Append this element to the task list.
-
-var createTaskHandler = function () {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+
+formEl.addEventListener("submit", createTaskHandler)
 
 
 
@@ -30,6 +23,44 @@ buttonEl.addEventListener("click", createTaskHandler);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//OLD COMMENTS FOR REFERENCE
+
+
+// Then we'll add the following event listener and also change the text of the new task item dynamically, using the textContent property we used previously to note, "This is a new task."
+
+
+// // Create a new task item.
+// // Style the new task item.
+// // Add the text.
+// // Append this element to the task list.
+// var createTaskHandler = function () {
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.textContent = "This is a new task.";
+//     tasksToDoEl.appendChild(listItemEl);
+// };
+
+// buttonEl.addEventListener("click", createTaskHandler);
 
 
 
