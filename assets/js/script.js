615111abaab846a72a0745fbd6bad5a6d2665da5
Mike <mike.levy.rva@gmail.com>
2022-06-16 16:53:57 -0400

    loading tasks from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -235,7 +235,33 @@ var deleteTask = function (taskId) {
 
 var saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+};
+
+
+
+var loadTasks = function () {
+    // Gets task items from localStorage.
+    // Converts tasks from the string format back into an array of objects.
+    // Iterates through a tasks array and creates task elements on the page from it.
+    var savedTasks = localStorage.getItem("tasks");
+    // if there are no tasks, set tasks to an empty array and return out of the function
+    if (!savedTasks) {
+        return false;
+    }
+    console.log("Saved tasks found!");
+    // else, load up saved tasks
+
+    // parse into array of objects
+    savedTasks = JSON.parse(savedTasks);
+    console.log(savedTasks)
+
+    // loop through savedTasks array
+    for (var i = 0; i < savedTasks.length; i++) {
+        // pass each task object into the `createTaskEl()` function
+        console.log(savedTasks[i])
+        createTaskEl(savedTasks[i]);
+    }
+};
 
 
 
@@ -247,3 +273,6 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+
+loadTasks();
